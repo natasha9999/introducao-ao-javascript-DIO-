228e158c0138a7f39a6ca29ff5b53a16c31be447
Natasha Alves <alvesna9999@gmail.com>
2022-07-07 19:05:08 -0300

    Exercício criando uma lista de tarefas

---
+++ lista-de-tarefas-DIO/assets/js/scripts.js
@@ -0,0 +1,6 @@
+const caixaDeTexto = document.querySelector("texto");
+const botaoAdicionar = document.querySelector("botao");
+
+function validarInput() {
+    //se o input estiver vazio, será considerado inválido e ficará em vermelho.
+}
