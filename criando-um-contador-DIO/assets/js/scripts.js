c312c688b46bc6fddea391478ebb30fe64e33391
Natasha Alves <alvesna9999@gmail.com>
2022-07-08 17:38:21 -0300

    Exercício lista de tarefas versão 2

--- criando-um-contador-DIO/assets/js/scripts.js
+++ criando-um-contador-DIO/assets/js/scripts.js
@@ -1,32 +1,26 @@
-var areaNumeroAtual = document.getElementById("numeroAtual");
-
+var areaNumeroAtual = document.getElementById('numeroAtual');
 var numeroAtual = 0;
 
-
 function aumenta() {
-    numeroAtual += 1;
+    numeroAtual = numeroAtual + 1;
     areaNumeroAtual.innerHTML = numeroAtual;
+
+    if (numeroAtual >= 0) {
+        document.getElementById("numeroAtual").style.color = "#ffffff";
+    } 
 }
 
 function diminui() {
-    numeroAtual -= 1;
+    numeroAtual = numeroAtual - 1;
     areaNumeroAtual.innerHTML = numeroAtual;
-}
 
-function mudaCor() {
     if (numeroAtual < 0) {
         document.getElementById("numeroAtual").style.color = "#ff0000";
-    } else if (numeroAtual >= 0) {
-        document.getElementById("numeroAtual").style.color = "#ffffff";
-    }
+    } 
 }
 
 function desabilitaBotao() {
-    if (numeroAtual <= -15) {
-        document.getElementById("btnMenos").disabled = true;
-    } else if (numeroAtual >= 20) {
-        document.getElementById("btnMais").disabled = true;
+    if (numeroAtual <= -10 || numeroAtual > 14) {
+        document.getElementsByName("subtrair").disabled = true;
     }
 }
-
-
--- lista-de-tarefas-DIO/assets/js/scripts.js
+++ lista-de-tarefas-DIO/assets/js/scripts.js
@@ -2,36 +2,8 @@ const caixaDeTexto = document.querySelector("texto");
 const botaoAdicionar = document.querySelector("botao");
 
 function validarInput() {
-    // Se o input estiver vazio, será considerado inválido e ficará em vermelho.
-    if (caixaDeTexto.ariaValueMax.trim().length > 0) {
-        return true;
-    } else {
-        return false;
-    }
+    //se o input estiver vazio, será considerado inválido e ficará em vermelho.
 }
 
-function adicionarTarefas() {
-    const inputEValido = validarInput();
-
-    // Se o input for inválido execute o if a baixo:
-    if (!inputEValido) {
-        caixaDeTexto.classList.add("error");
-    }
-
-    // Se houve alguma mudança no input, verifica novamente se o input é válido.
-    // Se for válido, retira o erro:
-    function mudancaDoInput() {
-        const inputEValido = validarInput();
-
-        if (inputEValido) {
-            return caixaDeTexto.classList.remove("error");
-        }
-    }
 
 
-    // Se o input for válido execute o código abaixo:
-}
-
-botaoAdicionar.addEventListener("click", adicionarTarefas());
-
-caixaDeTexto.addEventListener("change", mudancaDoInput());
