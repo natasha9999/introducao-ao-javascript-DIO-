6595b28b41fecdca99cf4ed4b1cd1a8d9c5ffede
Natasha Alves <alvesna9999@gmail.com>
2022-07-08 17:42:31 -0300

    Exercício lista de tarefas versão 3

--- lista-de-tarefas-DIO/assets/js/scripts.js
+++ lista-de-tarefas-DIO/assets/js/scripts.js
@@ -2,8 +2,35 @@ const caixaDeTexto = document.querySelector("texto");
 const botaoAdicionar = document.querySelector("botao");
 
 function validarInput() {
-    //se o input estiver vazio, será considerado inválido e ficará em vermelho.
+    // Se o input estiver vazio, será considerado inválido e ficará em vermelho.
+    if (caixaDeTexto.value.trim().length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+function adicionarTarefas() {
+    const inputEValido = validarInput();
 
+    // Se o input for válido executo o if abaixo:
+    if (!inputEValido) {
+        caixaDeTexto.classList.add("error");
+    }
 
+    // Se houver alguma mudança no input, verifica novamente se o input é válido.
+    // Se for válido, retira o erro:
+    function mudancaDoInput() {
+        const inputEValido = validarInput();
+
+        if (inputEValido) {
+            return caixaDeTexto.classList.remove("error");
+        }
+    }
+
+    // Se o input for válido execute o código abaixo:
+}
+
+botaoAdicionar.addEventListener("click", adicionarTarefas());
+
+caixaDeTexto.addEventListener("change", mudancaDoInput());
